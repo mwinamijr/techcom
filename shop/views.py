23b21c8c28d3be8c8b1ac67fc2987955010e46fd
mwinamijr <mwinamijr@gmail.com>
2021-02-07 13:16:19 +0300

    created products view

--- shop/views.py
+++ shop/views.py
@@ -1,1 +1,33 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
+
+from .products import products
+
+@api_view(['GET'])
+def getRoutes(request):
+    routes = [
+        '/api/products',
+        '/api/products/create/',
+        '/api/products/upload/',
+        '/api/products/<id>/reviews',
+        '/api/products/top/',
+        '/api/products/<id>/',
+        '/api/products/deelete/<id>/',
+        '/api/products/<update>/<id>/',
+    ]
+    return Response(routes)
+
+@api_view(['GET'])
+def getProducts(request):
+    return Response(products)
+
+@api_view(['GET'])
+def getProduct(request, pk):
+    product = None
+    for i in products:
+        if i['_id'] == pk:
+            product = i
+            break
+    return Response(product)
